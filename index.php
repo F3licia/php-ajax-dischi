d657e263475814426db939d1830af314fe12bd2e
F3licia <romeofelicia@gmail.com>
2021-06-03 23:04:21 +0200

    milestone 1 completata

--- index.php
+++ index.php
@@ -4,21 +4,16 @@ in pagina: al caricamento della pagina ci saranno tutti i dischi.-->
 <?php
 $data = include("./data/data.php");
 
-function getnPrint($data) {
- foreach($data as $element){
-     $poster = $element["poster"];
-     $title = $element["title"];
-     $author = $element["author"];
-     $genre = $element["genre"];
-     $year = $element["year"];
-    echo "<img src='$poster'>";
-    echo "<h2>$author</h2>";
-    echo "<h3>$title</h3>";
-    echo "<h5>$year</h5>";
-    echo "<p>$genre</p>";
-   };  
-};
-
+function getnPrint($element){
+   $poster = $element["poster"];
+   echo "<img src='$poster'>";
+   $author = $element["author"];
+   echo "<h2>$author</h2>";
+   $title = $element["title"];
+   echo "<h3>$title</h3>";
+   $year = $element["year"];
+   echo "<h5>$year</h5>";
+}
 
 ?>
 
@@ -28,12 +23,19 @@ function getnPrint($data) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css?v=<?php echo time(); ?>"> <!--aggiorna chace ogni volta-->
     <title>php dischi</title>
 </head>
 <body>
-
-<?php getnPrint($data) ?>
-   
-    
+    <div class="container">
+         <?php foreach($data as $element){ ?>
+            <div class="card">
+            <?php  
+                getnPrint($element);   
+               ?>
+            </div>
+        
+        <?php } ?>
+    </div>
 </body>
-</html>
+</html> 
